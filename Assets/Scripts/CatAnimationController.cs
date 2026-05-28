2387bb039f78bfeb5ce062e049657ad4c97e8b87
Hsu Wai Hnin Kyaw <hsuwaihninkyaw@gmail.com>
2026-05-27 18:12:26 -0700

    Add playable cat controller prototype (#7)
    * Implemented playable cat controller with third person camera and animations
    
    * Checkpoint before replacing cat model
    
    * new cat model

---
+++ Assets/Scripts/CatAnimationController.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+using StarterAssets;
+
+public class CatAnimationController : MonoBehaviour
+{
+    public CharacterController controller;
+    public StarterAssetsInputs input;
+
+    private Animator animator;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        float speed = controller.velocity.magnitude;
+
+        // Walk animation
+        animator.SetFloat("Speed", speed);
+
+        // Jump animation
+        if (input.jump)
+        {
+            animator.SetTrigger("Jump");
+        }
+    }
+}
