b19d90e6abe28482471b3ae80f03f0b8920ab098
XuanNhu <xuannhu1406@gmail.com>
2026-06-01 16:39:47 -0700

    Add open/close drawer interaction with E key

--- Assets/Brick Project Studio/_BPS Basic Assets/Common/Scripts and Animations/Drawer/X Axis/Drawer_Pull_X.cs
+++ Assets/Brick Project Studio/_BPS Basic Assets/Common/Scripts and Animations/Drawer/X Axis/Drawer_Pull_X.cs
@@ -1,74 +1,73 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 namespace SojaExiles
-
 {
+    public class Drawer_Pull_X : MonoBehaviour
+    {
+        public Animator pull_01;
+        public bool open;
+        public Transform Player;
+        public float interactDistance = 10f;
+        public GameObject interactPrompt;
+        private bool isAnimating = false;
 
-	public class Drawer_Pull_X : MonoBehaviour
-	{
+        void Start()
+        {
+            open = false;
+            if (interactPrompt != null)
+                interactPrompt.SetActive(false);
+        }
 
-		public Animator pull_01;
-		public bool open;
-		public Transform Player;
+        void Update()
+        {
+            if (Player)
+            {
+                float dist = Vector3.Distance(Player.position, transform.position);
 
-		void Start()
-		{
-			open = false;
-		}
+                if (dist <= interactDistance)
+                {
+                    if (interactPrompt != null)
+                        interactPrompt.SetActive(true);
 
-		void OnMouseOver()
-		{
-			{
-				if (Player)
-				{
-					float dist = Vector3.Distance(Player.position, transform.position);
-					if (dist < 10)
-					{
-						print("object name");
-						if (open == false)
-						{
-							if (Input.GetMouseButtonDown(0))
-							{
-								StartCoroutine(opening());
-							}
-						}
-						else
-						{
-							if (open == true)
-							{
-								if (Input.GetMouseButtonDown(0))
-								{
-									StartCoroutine(closing());
-								}
-							}
+                    if (Keyboard.current != null &&
+                        Keyboard.current.eKey.wasPressedThisFrame &&
+                        !isAnimating)
+                    {
+                        if (open == false)
+                            StartCoroutine(opening());
+                        else
+                            StartCoroutine(closing());
+                    }
+                }
+                else
+                {
+                    if (interactPrompt != null)
+                        interactPrompt.SetActive(false);
+                }
+            }
+        }
 
-						}
+        IEnumerator opening()
+        {
+            isAnimating = true;
+            print("you are opening the drawer");
+            pull_01.Play("openpull_01");
+            open = true;
+            yield return new WaitForSeconds(.5f);
+            isAnimating = false;
+        }
 
-					}
-				}
-
-			}
-
-		}
-
-		IEnumerator opening()
-		{
-			print("you are opening the door");
-			pull_01.Play("openpull_01");
-			open = true;
-			yield return new WaitForSeconds(.5f);
-		}
-
-		IEnumerator closing()
-		{
-			print("you are closing the door");
-			pull_01.Play("closepush_01");
-			open = false;
-			yield return new WaitForSeconds(.5f);
-		}
-
-
-	}
+        IEnumerator closing()
+        {
+            isAnimating = true;
+            print("you are closing the drawer");
+            pull_01.Play("closepush_01");
+            open = false;
+            yield return new WaitForSeconds(.5f);
+            isAnimating = false;
+        }
+    }
 }
